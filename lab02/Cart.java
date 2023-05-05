bf0d295910f991186b6abfefcf5e6b705cd9157b
BinhOiDungNghien <91303157+BinhOiDungNghien@users.noreply.github.com>
2023-05-05 23:44:46 +0700

    Add files via upload

---
+++ lab02/Cart.java
@@ -0,0 +1,45 @@
+package lab02;
+
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED = 20;
+
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBER_ORDERED - 1 ) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+            if (qtyOrdered == MAX_NUMBER_ORDERED - 1) {
+                System.out.println("The cart is almost full");
+            }
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getTitle() == disc.getTitle()) {
+                for (int j=i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j+1];
+                }
+                itemsOrdered[qtyOrdered-1] = null;
+                qtyOrdered--;
+            }
+        }
+    }
+
+    public float totalCost() {
+        float totalCost = 0.0f;
+        for (int i = 0; i < qtyOrdered; i++) {
+            totalCost += itemsOrdered[i].getCost();
+        }
+        return totalCost;   
+    }
+
+    public int getQty() {
+        return qtyOrdered;
+    }
+
+}
